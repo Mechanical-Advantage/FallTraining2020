3ee055c4cabec01612f225a9a077fdb6966bf504
V-Ra <23vinamra@gmail.com>
2020-10-14 19:27:49 -0400

    Test commit and push

--- src/main/java/frc/robot/SimulatorMain.java
+++ src/main/java/frc/robot/SimulatorMain.java
@@ -25,6 +25,6 @@ public final class SimulatorMain {
    * If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
-    ExternalSimLauncher.launch("trashcan.dyn.wpi.edu", 8889, Robot.class);
+    ExternalSimLauncher.launch("trashcan.dyn.wpi.edu", 24610, Robot.class);
   }
 }
