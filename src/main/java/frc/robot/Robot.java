422aa91282ad25c1d51188e318fd6e295f50d1b7
Keith White <M172108@na.merckgroup.com>
2020-10-24 18:36:32 -0400

    Add code for  joystick axis control

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.spinner.Spinner;
 import frc.robot.util.RobotIdentification;
 
 /**
@@ -108,6 +109,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    m_robotContainer.update();
   }
 
   @Override
