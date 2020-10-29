716766104ad5241e8ce8dfcb362d232e7ca399aa
Keith White <az2k@verizon.net>
2020-10-29 14:59:38 -0400

    Add code for runnign with joystick

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -110,6 +110,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     m_robotContainer.update();
+    CommandScheduler.getInstance().run();
   }
 
   @Override
