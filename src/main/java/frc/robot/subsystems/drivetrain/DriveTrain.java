68a2f0d9f2b549c7010bedae076d7eaf9238374a
Jonah Bonner <jonahwbonner@gmail.com>
2020-11-08 15:53:48 -0500

    Remove extra import

--- src/main/java/frc/robot/subsystems/drivetrain/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DriveTrain.java
@@ -8,7 +8,6 @@
 package frc.robot.subsystems.drivetrain;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import java.util.function.DoubleSupplier;
 
 public class DriveTrain extends SubsystemBase {
   private DriveTrainIO io;
